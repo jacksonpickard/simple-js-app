48f4dd9495ae5417a91d6d55b477ea510d9585c3
jacksonpickard <jackpickard2@gmail.com>
2023-01-13 11:02:11 -0700

    Pokemon list added w/ 3 Pokemon
    Name, Type, Height

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World')
-
-let favoriteFood = 'burger';
-document.write(favoriteFood)
+let pokemonList = [
+    {name: 'Charizard', height: 1.7, type: ['fire' , 'flying']},
+    {name: 'Oddish', height: 0.5, type: ['grass' , 'poison']},
+    {name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy']},
+];
