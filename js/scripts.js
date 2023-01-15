14c721c3cc446e73b24630b198bfb52e659366e4
jacksonpickard <jackpickard2@gmail.com>
2023-01-15 16:42:29 -0700

    Added 'for' loop and made largest pokemon have a message attached

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [
     {name: 'Oddish', height: 0.5, type: ['grass' , 'poison']},
     {name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy']},
 ];
+
+for (let i = 0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" );
+if (pokemonList[i].height >= 1.5){
+    document.write("Wow, that's big!!")
+    }
+}
