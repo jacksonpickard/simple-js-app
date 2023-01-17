4ba02803da48f304c84b116e52e5a2d69ada09a5
jacksonpickard <jackpickard2@gmail.com>
2023-01-17 13:42:12 -0700

    Updated for(each) loop with iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 
-let pokemonList = [
-    {name: 'Charizard', height: 1.7, type: ['fire' , 'flying']},
-    {name: 'Oddish', height: 0.5, type: ['grass' , 'poison']},
-    {name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy']},
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Charizard', height: 1.7, type: ['fire' , 'flying']},
+        {name: 'Oddish', height: 0.5, type: ['grass' , 'poison']},
+        {name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy']},
+    ];
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add,
+    getAll: getAll
+    };
+})();
 
 // for (let i = 0; i<pokemonList.length; i++){
 //     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
@@ -12,6 +27,6 @@ let pokemonList = [
 //     }
 // }
 
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
     document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
 });
