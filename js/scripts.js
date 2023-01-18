05966c671786e467e7df2fee50f50085079bd71c
jacksonpickard <jackpickard2@gmail.com>
2023-01-18 13:04:56 -0700

    added ID and typeof === object

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 
 let pokemonRepository = (function () {
+
     let pokemonList = [
-        {name: 'Charizard', height: 1.7, type: ['fire' , 'flying']},
-        {name: 'Oddish', height: 0.5, type: ['grass' , 'poison']},
-        {name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy']},
-    ];
+        { ID: "1", name: 'Charizard', height: 1.7, type: ['fire' , 'flying'], ability: "blaze "},
+        { ID: "2", name: 'Oddish', height: 0.5, type: ['grass' , 'poison'], ability: "overgrow" },
+        { ID: "3", name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy'], ability:"torrent" },
+    ]
 
 function add(pokemon) {
+    if (
+        typeof pokemon === "object" &&
+        "ID" in pokemon &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "type" in pokemon &&
+        "ability" in pokemon
+) {
     pokemonList.push(pokemon);
+} else {
+    console.log("pokemon is not correct");
+}
 }
 
 function getAll() {
@@ -20,13 +32,17 @@ return {
     };
 })();
 
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
+});
+
+
+// for loop: was made obsolete by forEach loop
+
 // for (let i = 0; i<pokemonList.length; i++){
 //     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
 // if (pokemonList[i].height >= 1.5){
 //     document.write(" Wow, that's big!!") + "</p>"
 //     }
 // }
-
-pokemonRepository.getAll().forEach(function(pokemon){
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
-});
