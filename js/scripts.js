b4e803e044ab20fc511ef21e7da0c716f906d7ac
jacksonpickard <jackpickard2@gmail.com>
2023-01-27 12:18:19 -0700

    cleaned up scripts.js before task 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,14 @@ function getAll() {
 
 function addListItem (pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
-   let listPokemon = document.createElement('li');
-   let button = document.createElement('button');
-   button.innerText = pokemon.name;
-   button.classList.add("button-class");
-   listPokemon.appendChild(button);
-   pokemonList.appendChild(listPokemon);
-   button.addEventListener("click", 
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    button.addEventListener("click", 
+    
 function(Event) {
     showDetails(pokemon);
    });
